b898c5d3dd56e14695585fd9a70e6f9338957b58
ksngo <sevenelectrons@gmail.com>
2020-04-24 09:27:42 +0000

    how to add index in jinjja?

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import pymongo
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
+from jinja2 import Template
 load_dotenv()
 
 MONGO_URI = os.environ.get('MONGO_URI')
@@ -14,11 +15,41 @@ client =pymongo.MongoClient(MONGO_URI)
 app = Flask(__name__)
 
 @app.route('/')
-def show_recipes():
+def index():
 
     get_recipes = client[DB_NAME].recipes.find()
 
     return render_template('index.html', get_recipes = get_recipes)
+
+
+@app.route('/<username>/add_recipe/')
+def add_recipe(username) :
+
+    username=username
+
+    return render_template('add_my_recipe.html', username=username)
+
+
+
+@app.route('/recipes/')
+def recipes():
+
+    get_recipes = client[DB_NAME].recipes.find()
+    # get_average_likes = client[DB_NAME].recipes.aggregate(
+    #     [
+    #         {
+    #          $group:
+    #             {
+    #              _id: "$item",
+    #              avglikes: {$avg: "$likes"}
+    #             }
+    #         }
+    #     ]
+    # )
+
+    return render_template('public_all_recipes.html', results=get_recipes )
+
+
 
 
 if __name__ == '__main__':
---
+++ static/index.js
@@ -0,0 +1,29 @@
+
+console.log('success')
+
+$(function(){
+document.getElementById('add-row').addEventListener('click',addRow)
+
+})
+
+
+
+
+
+    
+
+function addRow() {
+
+    console.log('success in addrow')
+    let rowElement = document.createElement('div')
+    rowElement.innerHTML = `
+        <input type='text' name='step1' placeholder='insert step'/>`
+    
+    document.getElementById('add-recipe').appendChild(rowElement)
+
+}
+
+
+
+
+
