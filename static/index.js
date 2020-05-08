b0cd6e6e093b79d631dc3736f1e62e079f3ab83c
ksngo <sevenelectrons@gmail.com>
2020-05-08 12:24:27 +0000

    Insert sample recipe pops out in index page.

--- static/index.js
+++ static/index.js
@@ -15,6 +15,9 @@ $(function (){
         "url(/static/images/cooking.jpg)"
 
     )
+    
+
+    setInterval(switchImage, 5000)
 
 })
 
@@ -22,14 +25,17 @@ let counter =0
 
 function switchImage () {
 
+    if (document.getElementById("sample-body").style.display == ("" || "none")) {
     counter++
     counter = counter % bgImageArray.length 
     console.log(counter)
 
     document.querySelector('body').style.backgroundImage = bgImageArray[counter]
+
+    } 
 }
 
-setInterval(switchImage, 5000)
+
 
 $(function(){
 
@@ -42,13 +48,15 @@ $(function(){
 function showRecipeSample() {   
 
     document.getElementById("sample-body").style.display = "block"
-    // document.getElementsByTagName("body")[0].style.height = "10px"
+    document.getElementsByTagName('body')[0].style.backgroundImage = 'url(/static/images/cook2-sepia.jpg)'
+    
     
 }
 
 function closeRecipeSample() {
 
     document.getElementById("sample-body").style.display = "none"
-    // document.getElementsByTagName("body")[0].style.height = "100vh"
+    document.getElementsByTagName('body')[0].style.backgroundImage = 'url(/static/images/cook2.jpg)'
+    
 
 }
